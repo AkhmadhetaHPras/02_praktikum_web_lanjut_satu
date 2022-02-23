2c65e0a81ed9142e553a51aad8b6cdb8d1ea799c
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-02-23 15:59:30 +0700

    Praktikum 2 satu controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,13 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Praktikum 1
-Route::get('/', function () {
-    echo "Selamat Datang";
-});
-Route::get('/about', function () {
-    echo "Nama : Akhmadheta Hafid Prasetyawan";
-    echo "<br>NIM : 2041720221";
-});
-Route::get('/articles/{id}', function ($id) {
-    echo "Halaman Artikel dengan ID {$id}";
-});
+// Route::get('/', function () {
+//     echo "Selamat Datang";
+// });
+// Route::get('/about', function () {
+//     echo "Nama : Akhmadheta Hafid Prasetyawan";
+//     echo "<br>NIM : 2041720221";
+// });
+// Route::get('/articles/{id}', function ($id) {
+//     echo "Halaman Artikel dengan ID {$id}";
+// });
+
+// Praktikum 2 satu controller
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
