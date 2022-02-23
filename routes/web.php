5d0dccc41398240d8807ca7157293db238a3017c
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-02-23 16:48:35 +0700

    Praktikum 3 halaman home dan product(route prefix)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,6 +35,17 @@ use App\Http\Controllers\ArticleController;
 // Route::get('/articles/{id}', [PageController::class, 'articles']);
 
 // Praktikum 2 tiga controller (single action controller)
-Route::get('/', [HomeController::class,'index']);
-Route::get('/about', [AboutController::class,'about']);
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+// Route::get('/', [HomeController::class,'index']);
+// Route::get('/about', [AboutController::class,'about']);
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+// Praktikum 3
+Route::get('/', [HomeController::class,'index']); //NO 1
+// Route prefix
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', [ProductController::class, 'eduGames']);
+    Route::get('/marbel-and-friends-kids-games', [ProductController::class, 'kidsGames']);
+    Route::get('/riri-story-books', [ProductController::class, 'storyBooks']);
+    Route::get('/kolak-kids-songs', [ProductController::class, 'kidsSongs']);
+});
+    
