8dba45e1851f44d5acc3bd71ea4cabdee3e0c7c3
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-03-02 12:34:11 +0700

    reset route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\ProgramController;
-use App\Http\Controllers\AboutUsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,48 +13,26 @@ use App\Http\Controllers\AboutUsController;
 |
 */
 
-// Praktikum 1
-// Route::get('/', function () {
-//     echo "Selamat Datang";
-// });
-// Route::get('/about', function () {
-//     echo "Nama : Akhmadheta Hafid Prasetyawan";
-//     echo "<br>NIM : 2041720221";
-// });
-// Route::get('/articles/{id}', function ($id) {
-//     echo "Halaman Artikel dengan ID {$id}";
+
+Route::get('/', [HomeController::class,'index']);
+// // Route prefix NO 2
+// Route::prefix('category')->group(function () {
+//     Route::get('/marbel-edu-games', [ProductController::class, 'eduGames']);
+//     Route::get('/marbel-and-friends-kids-games', [ProductController::class, 'kidsGames']);
+//     Route::get('/riri-story-books', [ProductController::class, 'storyBooks']);
+//     Route::get('/kolak-kids-songs', [ProductController::class, 'kidsSongs']);
 // });
 
-// Praktikum 2 satu controller
-// Route::get('/', [PageController::class,'index']);
-// Route::get('/about', [PageController::class,'about']);
-// Route::get('/articles/{id}', [PageController::class, 'articles']);
+// // Route param NO 3
+// Route::get('/news', [NewsController::class, 'listNews']);
+// Route::get('/news/{title}', [NewsController::class, 'news']);
 
-// Praktikum 2 tiga controller (single action controller)
-// Route::get('/', [HomeController::class,'index']);
-// Route::get('/about', [AboutController::class,'about']);
-// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+// // Route prefix NO 4
+// Route::prefix('program')->group(function () {
+//     Route::get('/karir', [ProgramController::class, 'karir']);
+//     Route::get('/magang', [ProgramController::class, 'magang']);
+//     Route::get('/kunjungan-industri', [ProgramController::class, 'kunjungan']);
+// });
 
-// Praktikum 3
-Route::get('/', [HomeController::class,'index']); //NO 1
-// Route prefix NO 2
-Route::prefix('category')->group(function () {
-    Route::get('/marbel-edu-games', [ProductController::class, 'eduGames']);
-    Route::get('/marbel-and-friends-kids-games', [ProductController::class, 'kidsGames']);
-    Route::get('/riri-story-books', [ProductController::class, 'storyBooks']);
-    Route::get('/kolak-kids-songs', [ProductController::class, 'kidsSongs']);
-});
-
-// Route param NO 3
-Route::get('/news', [NewsController::class, 'listNews']);
-Route::get('/news/{title}', [NewsController::class, 'news']);
-
-// Route prefix NO 4
-Route::prefix('program')->group(function () {
-    Route::get('/karir', [ProgramController::class, 'karir']);
-    Route::get('/magang', [ProgramController::class, 'magang']);
-    Route::get('/kunjungan-industri', [ProgramController::class, 'kunjungan']);
-});
-
-// Route biasa NO 5
-Route::get('/about-us', [AboutUsController::class, 'index']);
+// // Route biasa NO 5
+// Route::get('/about-us', [AboutUsController::class, 'index']);
