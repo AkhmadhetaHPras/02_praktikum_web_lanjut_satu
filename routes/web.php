ff4d547041efc8559cc22a897e087a7b637b3325
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-04-20 11:06:55 +0700

    Add upload image in cms

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DinningController;
 use App\Http\Controllers\GalleryController;
@@ -23,13 +24,14 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/', [HomeController::class,'index']);
-Route::get('/about', [AboutController::class,'index']);
-Route::get('/contact', [ContactController::class,'index']);
-Route::get('/dinning', [DinningController::class,'index']);
-Route::get('/gallery', [GalleryController::class,'index']);
-Route::get('/news', [NewsController::class,'index']);
-Route::get('/rooms', [RoomController::class,'index']);
-Auth::routes();
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/dinning', [DinningController::class, 'index']);
+Route::get('/gallery', [GalleryController::class, 'index']);
+Route::get('/news', [NewsController::class, 'index']);
+Route::get('/rooms', [RoomController::class, 'index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('articles', ArticleController::class);
